e1949beaee5f7448fae646415505b4228b926141
edinabwari <bwarikwambokaedina@gmail.com>
2024-06-14 11:05:32 +0300

    updated readme

--- Sudoku_Solve.py
+++ Sudoku_Solve.py
@@ -1,21 +1,17 @@
 import numpy as np
-
 # Function to check if a number is a possibility in a given position
 def possibility(row, column, num, sudoku_puzzle):
     # Check if the number is not in the current row and column
     for i in range(9):
         if sudoku_puzzle[row][i] == num or sudoku_puzzle[i][column] == num:
-            return False
-    
+            return False  
     # Check if the number is not in the current 3x3 grid
     x0, y0 = (column // 3) * 3, (row // 3) * 3
     for i in range(3):
         for j in range(3):
             if sudoku_puzzle[y0 + i][x0 + j] == num:
-                return False
-    
+                return False   
     return True
-
 # Function to solve the Sudoku puzzle using backtracking
 def solve_sudoku(sudoku_puzzle):
     for row in range(9):
@@ -29,11 +25,9 @@ def solve_sudoku(sudoku_puzzle):
                         sudoku_puzzle[row][column] = 0
                 return False
     return True
-
 # Function to display the Sudoku puzzle
 def print_sudoku(sudoku_puzzle):
     print(np.matrix(sudoku_puzzle))
-
 # User input for the Sudoku puzzle
 sudoku_puzzle = []
 for i in range(9):
